d2176f9993fe46cf33b10fb7db97dddac0fc0d2b
abrakib942 <abrakib942@gmail.com>
2022-05-22 22:56:14 +0600

    get api

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.estsi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const toolCollection = client.db("carpenter-tools").collection("tools");
+
+    app.get("/tool", async (req, res) => {
+      const tools = await toolCollection.find({}).toArray();
+
+      res.send(tools);
+    });
+
+    app.get("/tool/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tool = await toolCollection.findOne(query);
+      res.send(tool);
+    });
+  } finally {
+    //
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("hello from carpenter tools");
