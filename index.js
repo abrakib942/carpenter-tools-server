692bf9cfafe0bd3a24fba3267aa39380021a9fda
abrakib942 <abrakib942@gmail.com>
2022-05-24 02:31:40 +0600

    order api with email query

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
   try {
     await client.connect();
     const toolCollection = client.db("carpenter-tools").collection("tools");
+    const orderCollection = client.db("carpenter-tools").collection("orders");
 
     app.get("/tool", async (req, res) => {
       const tools = await toolCollection.find({}).toArray();
@@ -32,6 +33,33 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const tool = await toolCollection.findOne(query);
       res.send(tool);
+    });
+
+    app.put("/tool/:id", async (req, res) => {
+      const id = req.params.id;
+      const orderQuantity = req.body;
+
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: { orderQuantity },
+      };
+      const options = { upsert: true };
+      const result = await toolCollection.updateOne(filter, updateDoc, options);
+
+      res.send(result);
+    });
+
+    app.post("/order", async (req, res) => {
+      const purchase = req.body;
+      const result = await orderCollection.insertOne(purchase);
+      res.send(result);
+    });
+
+    app.get("/order", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const orders = await orderCollection.find(query).toArray();
+      res.send(orders);
     });
   } finally {
     //
