7353517138d18ac05add5fc2f78ae6282c06d7ec
abrakib942 <abrakib942@gmail.com>
2022-05-26 14:41:09 +0600

    delete review api

--- index.js
+++ index.js
@@ -106,6 +106,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(filter);
+      res.send(result);
+    });
+
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
